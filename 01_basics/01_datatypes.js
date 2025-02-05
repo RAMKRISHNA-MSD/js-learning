edd43f993fb7a042e7c49d388bb72ab9ab5a0df0
RAMKRISHNA_MSD <168726057+RAMKRISHNA-MSD@users.noreply.github.com>
2025-02-05 05:57:06 +0000

    datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,20 @@
+let name="sai kiran"
+let age=22
+let lover=false
+let heart=null
+let marraige;
+
+// Number 2 to pw of 53
+// BigInt
+// String 
+// Boolean
+// null
+// Symbol
+
+let score="33"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+
+
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,5 @@
+const accId=36748
+let accName="sai kiran"
+var accCity="kubeer"
+let accholderLover;
+console.table([accId,accName,accCity,accholderLover])
